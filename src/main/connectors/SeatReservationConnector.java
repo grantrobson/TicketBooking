693b2d8423cf760eda2b2de3531e3bbecf79480d
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-03-22 13:50:12 +0000

    Add some comments

--- src/main/connectors/SeatReservationConnector.java
+++ src/main/connectors/SeatReservationConnector.java
@@ -5,6 +5,7 @@ import models.*;
 import java.util.List;
 
 public interface SeatReservationConnector {
+    // Presumably this would be calling the external service via a HTTP GET or POST: Could use a Future here
     SeatReservationResult reserve(List<TicketTypeRequest> ttrList);
     SeatReservationResponseCode cancelReservations(List<String> seats);
 
--- src/main/connectors/TicketPaymentConnector.java
+++ src/main/connectors/TicketPaymentConnector.java
@@ -4,5 +4,6 @@ import models.TicketPaymentRequest;
 import models.TicketPaymentResult;
 
 public interface TicketPaymentConnector {
+    // Presumably this would be calling the external service via a HTTP GET or POST: Could use a Future here
     TicketPaymentResult makePayment(TicketPaymentRequest ticketPaymentRequest);
 }
